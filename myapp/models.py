bc16842ac281be67829502c2fc86f022e94dfb1f
Astlejoe789 <astlejoe789@gmail.com>
2025-03-12 11:51:20 +0530

    https://github.com/Astlejoe789/login-page-web-project3.git

---
+++ myapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class user(models.Model):
+    username=models.CharField(max_length=100)
+    email=models.EmailField()
+    password=models.CharField(max_length=100)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+
+    def _str_(self):
+        return self.username 
